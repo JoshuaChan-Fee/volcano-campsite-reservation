184f367420a1eed0a62cbd3b299a3f6704bacd67
JoshuaChan-Fee <joshuachanfee@gmail.com>
2022-10-27 13:03:10 -0400

    Initial spring-boot application

---
+++ src/main/java/com/upgrade/volcanocampsitereservation/VolcanoCampsiteReservationApplication.java
@@ -0,0 +1,12 @@
+package com.upgrade.volcanocampsitereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolcanoCampsiteReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolcanoCampsiteReservationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/upgrade/volcanocampsitereservation/VolcanoCampsiteReservationApplicationTests.java
@@ -0,0 +1,8 @@
+package com.upgrade.volcanocampsitereservation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolcanoCampsiteReservationApplicationTests {
+
+}
